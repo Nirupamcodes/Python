4ee89f4795d3a99bad08acece11589607cc99388
Nirupamcodes <nirupamgawali@gmail.com>
2023-03-21 10:54:04 +0530

    python day 5

--- assignment_3.1_factorial.py
+++ assignment_3.1_factorial.py
@@ -1,7 +1,19 @@
 #n = (int(input("enter a number - ")))
-num = 5
+"""num = 5
 factorial = 1
 for i in range(1,num + 1):
        factorial = factorial*i
-print("The factorial of",num,"is",factorial)
+print("The factorial of",num,"is",factorial)"""
 
+
+
+def factorial(n):
+    
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n*factorial(n-1)
+num = int(input("enter a number - "))
+result = factorial(num)
+print("the factorial of number is ")
+print(result)
--- assignment_3.4_dublicate_number.py
+++ assignment_3.4_dublicate_number.py
@@ -1,9 +1,25 @@
 #WAP to remove dublicate elements from list
+# using set
 
-l = [22,34,43,54,22,34,55,54]
+"""l = [22,34,43,54,22,34,55,54]
 print("orignal list")
 print(l)
 
 n = set(l)
 print("new list")
+print(n)"""
+
+l = [22,34,43,54,22,34,55,54]
+
+print("orignal list")
+print(l)
+n = []
+
+for i in l:
+    if i not in n:
+        n.append(i)
+print("new list without dublicate elements")
 print(n)
+
+
+
